e96f51f52fe31e1e2cdcd682ab17cfe6fa4f6620
chrisdadev13 <chrisdadev13@gmail.com>
2025-06-21 22:19:21 +0200

    fix: theme provider marketing page

--- apps/mail/providers/client-providers.tsx
+++ apps/mail/providers/client-providers.tsx
@@ -1,21 +1,36 @@
 import { NuqsAdapter } from 'nuqs/adapters/react-router/v7';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { PostHogProvider } from '@/lib/posthog-provider';
+import { LoadingProvider } from '@/components/context/loading-context';
+import { useSettings } from '@/hooks/use-settings';
 import { Provider as JotaiProvider } from 'jotai';
 import type { PropsWithChildren } from 'react';
 import Toaster from '@/components/ui/toast';
+import { ThemeProvider } from 'next-themes';
 
 export function ClientProviders({ children }: PropsWithChildren) {
+  const { data } = useSettings();
+
+  const theme = data?.settings.colorTheme || 'system';
 
   return (
     <NuqsAdapter>
       <JotaiProvider>
-        <SidebarProvider>
-          <PostHogProvider>
-            {children}
-            <Toaster />
-          </PostHogProvider>
-        </SidebarProvider>
+        <ThemeProvider
+          attribute="class"
+          enableSystem
+          disableTransitionOnChange
+          defaultTheme={theme}
+        >
+          <SidebarProvider>
+            <PostHogProvider>
+              <LoadingProvider>
+                {children}
+                <Toaster />
+              </LoadingProvider>
+            </PostHogProvider>
+          </SidebarProvider>
+        </ThemeProvider>
       </JotaiProvider>
     </NuqsAdapter>
   );
